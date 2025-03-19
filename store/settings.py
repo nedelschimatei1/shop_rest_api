c6c2b88b62f7f4a6beec535b06f22c3414b55503
Matei <nedelschi.matei@gmail.com>
2025-03-20 00:31:12 +0200

    update commit

--- store/settings.py
+++ store/settings.py
@@ -161,6 +161,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute',
+    }
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated'
     # ]
